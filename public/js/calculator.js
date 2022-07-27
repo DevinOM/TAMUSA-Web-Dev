a5c73e2b07377b6c6dae981c719b310f86619158
Devin Madrigal <devinmadr210@gmail.com>
2022-07-27 10:02:39 -0500

    Add files via upload

---
+++ public/js/calculator.js
@@ -0,0 +1,25 @@
+function buttonNum(num) {
+    document.getElementById("result").value += num
+}
+
+function clearResult() {
+    document.getElementById("result").value = ""
+}
+
+function calculateResult() {
+    let input = document.getElementById("result").value 
+    let result = eval(input)
+
+    document.getElementById("result").value = result
+}
+
+function plusMinus() {
+    let num = document.getElementById("result").value
+    let result = eval(num * -1)
+
+    document.getElementById("result").value = result
+}
+
+function addOperator(operator) {
+    document.getElementById("result").value += operator
+}
